5c03a753f9afed66e98f85f06753b4049d8e9d36
Colin Rieger <rieger1085@gmail.com>
2017-11-07 22:42:24 -0700

    Name entry dialog, member list, and layout updates.

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,77 +1,81 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
+import {Socket, Presence} from "phoenix"
 
-// To use Phoenix channels, the first step is to import Socket
-// and connect at the socket path in "lib/web/endpoint.ex":
-import {Socket} from "phoenix"
+var socket = null;
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+function connectSocket(userId)
+{
+  socket = new Socket("/socket", {
+    params: { user_id: userId }
+  })
+  socket.connect();
+}
 
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/2" function
-// in "lib/web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, pass the token on connect as below. Or remove it
-// from connect if you don't care about authentication.
+function joinChannel(room)
+{
+  let channel       = socket.channel(room, {});
+  let messageInput  = document.querySelector("#message-input");
+  let messages      = document.querySelector("#messages");
+  let presences     = {};
+  
+  messageInput.addEventListener("keypress", event => {
+    if (event.keyCode === 13)
+    {
+      channel.push("message", {body: messageInput.value});
+      messageInput.value = "";
+    }
+  })
+  
+  channel.on("message", payload => {
+    let messageItem = document.createElement("li");
+    messageItem.innerText = `[${new Date().toLocaleTimeString()}] ${payload.user_id}: ${payload.body}`;
+    messages.appendChild(messageItem);
+  })
+  
+  channel.on("presence_state", state => {
+    presences = Presence.syncState(presences, state);
+    setMembers(presences);
+  })
+  
+  channel.on("presence_diff", diff => {
+    presences = Presence.syncDiff(presences, diff);
+    setMembers(presences);
+  })
+  
+  channel.join()
+    .receive("ok", resp => { console.log("Joined " + room + " successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join " + room, resp) });
+}
 
-socket.connect()
+function setMembers(presences) {
+  let members = document.querySelector("#members");
+  members.innerHTML = "";
 
-let channel       = socket.channel("room:general", {})
-let messageInput  = document.querySelector("#message-input")
-let messages      = document.querySelector("#messages")
+  Presence.list(presences, (id, {metas: [first, ...rest]}) => {
+    for (let i = 0; i <= rest.length; i++)
+    {
+      let memberItem = document.createElement("li");
+      memberItem.innerText = `${id}`;
+      members.appendChild(memberItem);
+    }
+  });
+}
 
-messageInput.addEventListener("keypress", event => {
-  if (event.keyCode === 13) {
-    channel.push("message", {body: messageInput.value})
-    messageInput.value = ""
-  }
-})
+$("#name-dialog").dialog({
+  dialogClass: "no-close",
+  autoOpen : false,
+  modal : true,
+  buttons: [
+    {
+      text: "Join",
+      click: function() {
+        let nameInput = document.querySelector("#name-input");
+        connectSocket(nameInput.value);
+        joinChannel("room:general");
+        $(this).dialog("close");
+      }
+    }
+  ]
+});
+$("#name-dialog").dialog('open');
 
-channel.on("message", payload => {
-  let messageItem = document.createElement("li");
-  messageItem.innerText = `${new Date().toLocaleTimeString()}: ${payload.body}`
-  messages.appendChild(messageItem)
-})
-
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
-
-export default socket
+export default socket;
--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -1,15 +1,26 @@
 defmodule ChatWeb.RoomChannel do
   use Phoenix.Channel
+  alias ChatWeb.Presence
 
   def join("room:general", _message, socket) do
+    send(self(), :after_join)
     {:ok, socket}
   end
+
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
+
+  def handle_info(:after_join, socket) do
+    push socket, "presence_state", Presence.list(socket)
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
+      online_at: inspect(System.system_time(:seconds))
+    })
+    {:noreply, socket}
+  end
   
   def handle_in("message", %{"body" => body}, socket) do
-    broadcast! socket, "message", %{body: body}
+    broadcast! socket, "message", %{user_id: socket.assigns.user_id, body: body}
     {:noreply, socket}
   end
 end
